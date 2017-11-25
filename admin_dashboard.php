503adaf3290ae6b549320ead9467508512e4d224
nanaadjoabentil <nana.bentil@outlook.com>
2017-11-25 15:34:35 +0000

    3:34 pm sat nov 25

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -1,38 +1,6 @@
 <?php
-require_once '../ApplicationFunctions.php';
- // transactions();
- // displayInfo();
-
-//  if (isset($_POST['search']))
-//  {
-//    search();
-//  }
-//
-//  public function search($number)
-//  {
-//    $db = Database::getInstance();
-//    try{
-//      $sql = $db->prepare("SELECT * FROM transactions WHERE number = :number");
-//      $sql->bindParam(":number",$number);
-//      $sql->execute();
-//     $res = $sql->fetch(PDO::FETCH_ASSOC);
-//     if($res !== FALSE)
-//     {
-//       echo $res['number'] . '<br />';
-//       echo $res['transaction_type'] . '<br />';
-//       echo $res['network'] . '<br />';
-//       echo $res['recipientcol'] . '<br />';
-//       echo $res['amountcol'] . '<br />';
-//       echo $res['confirmcol'] . '<br />';
-//     }
-//   }
-//   catch (PDOException $e)
-//    {
-//     #echo $e->getMessage();
-//     return NULL;
-//   }
-//  }
-// ?>
+require_once 'ApplicationFunctions.php';
+ ?>
 
 <!DOCTYPE html>
 <html>
@@ -40,8 +8,11 @@ require_once '../ApplicationFunctions.php';
        <!--Import Google Icon Font-->
        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
        <!--Import materialize.css-->
-       <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+       <link type="text/css" rel="stylesheet" href="materialize.min.css"  media="screen,projection"/>
+       <link type="text/css" rel="stylesheet" href="materialize.css"  media="screen,projection"/>
        <link type="text/css" rel="stylesheet" href="admin_dashboard.css"  media="screen,projection"/>
+       <!-- Compiled and minified CSS online-->
+       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
 
        <!--Let browser know website is optimized for mobile-->
        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
@@ -51,7 +22,9 @@ require_once '../ApplicationFunctions.php';
 
   <!--Import jQuery before materialize.js-->
      <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-     <script type="text/javascript" src="js/materialize.min.js"></script>
+     <script type="text/javascript" src="materialize.min.js"></script>
+     <!-- Compiled and minified JavaScript -->
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
 
   <h3> Admin Dashboard </h3>
 
